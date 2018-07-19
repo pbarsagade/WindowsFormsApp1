fe496ed762198bfab71afeec4f724d652edd4c1f
Prakash Barsagade <prakash.barsagade@outlook.com>
2018-07-19 10:40:49 +0530

    log4net added

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -20,8 +17,6 @@ namespace WindowsFormsApp1
             Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
-            //string fileContents = File.ReadAllText(string.Empty);
-            //do something with the file contents
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
@@ -31,12 +26,14 @@ namespace WindowsFormsApp1
         {
             // Log the exception, display it, etc
             Debug.WriteLine(e.Exception.Message);
+            log.Error(e.Exception.Message);
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             // Log the exception, display it, etc
             Debug.WriteLine((e.ExceptionObject as Exception).Message);
+            log.Error((e.ExceptionObject as Exception).Message);
         }
     }
 }
